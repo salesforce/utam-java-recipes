be63f1a75f4d858148668260e5c16c3737c21979
Elizaveta Ivanova <elizaveta.ivanova@salesforce.com>
2021-12-08 16:45:50 -0800

    test utam.dev

--- utam-tests/src/test/java/utam/examples/portal/UtamPortalTests.java
+++ utam-tests/src/test/java/utam/examples/portal/UtamPortalTests.java
@@ -29,9 +29,15 @@ public class UtamPortalTests extends UtamWebTestBase {
 
   @Test
   public void testMenu() {
+    log("Navigate to portal");
     getDriver().get("https://utam.dev");
+
+    log("Load Home Page");
     UtamDevHome homePage = from(UtamDevHome.class);
-    Assert.assertTrue(homePage.getMenuItems().size() == 3);
+
+    Assert.assertTrue(homePage.getMenuItems().size() == 3, "number of menu items");
+    homePage.getGrammarMenuItem().click();
+    Assert.assertEquals(getDomDocument().getUrl(), "https://utam.dev/grammar/spec");
   }
 
   @AfterTest
