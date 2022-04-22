9c3fb2b5dc7cddefeb916290f8cf10318ecc9c83
helenren <qren@salesforce.com>
2022-04-22 16:48:54 -0700

    add the loader config file back

--- utam-tests/src/test/java/utam/base/UtamMobileTestBase.java
+++ utam-tests/src/test/java/utam/base/UtamMobileTestBase.java
@@ -53,7 +53,7 @@ public abstract class UtamMobileTestBase {
   }
 
   private void setLoaderConfig() {
-    config = new UtamLoaderConfigImpl("utam-salesforceapp-pageobjects.config.json");
+    config = new UtamLoaderConfigImpl("loader.config.json");
     config.setProfile(MobilePlatformType.fromDriver(driver));
     loader = new UtamLoaderImpl(config, driver);
   }
@@ -70,7 +70,7 @@ public abstract class UtamMobileTestBase {
     setupMobile();
     System.setProperty("ios.device", "iPhone 8 Plus");
     System.setProperty("ios.app", getUserHomeRelativePath("SApp.app"));
-    desiredCapabilities.setDesiredCapability(MobileCapabilityType.PLATFORM_VERSION, "13.4");
+    desiredCapabilities.setDesiredCapability(MobileCapabilityType.PLATFORM_VERSION, "15.2");
     driver = WebDriverFactory.getWebDriver(DriverType.ios, appiumService, desiredCapabilities);
     setLoaderConfig();
   }
